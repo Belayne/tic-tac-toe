05d87ee1b221f58acf34785b1fca4dfdf553f0de
Naol <37399395+Belayne@users.noreply.github.com>
2023-08-14 11:07:40 +0200

    Add game and UI logic, missing win condition
    Draw the rest of the owl

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
 const Gameboard = (() => {
-    const board = ["X","X","X","O","O","O","X","X","X"];
+    const board = ["","","","","","","","",""];
 
-    const createGrid = (cont) => {
+    const addMark = (index, mark) => {
+        board[index] = mark;
+    }
+
+    const getBoard = () => board;
+
+    return {
+        getBoard,
+        addMark
+    }
+})();
+
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+
+    return {getName, getMark}
+}
+
+const GameController = (() => {
+    const playerOne = Player("One", "X")
+    const playerTwo = Player("Two", "O")
+    const board = Gameboard.getBoard();
+
+    let currentPlayer = playerOne;
+
+    const switchPlayer = () => {
+        currentPlayer = (currentPlayer == playerOne)? playerTwo: playerOne;
+        console.log(currentPlayer.getName())
+    }
+
+    const playRound = (index) => {
+        if(!board[index]) {
+            Gameboard.addMark(index, currentPlayer.getMark());
+            switchPlayer();
+        }
+        else {
+            console.log("Already Played")
+        }
+        console.log(Gameboard.getBoard())
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    return {
+        getCurrentPlayer,
+        playRound
+    }
+
+})();
+
+const DisplayController = (() => {
+    const boardDiv = document.querySelector(".board")
+    const messageDiv = document.querySelector(".message");
+
+    const createGrid = (() => {
         for(let i = 0; i < 9; i++){
             let gridItem = document.createElement('div');
             gridItem.classList.add('grid-item')
             gridItem.setAttribute('data-id', i)
-            gridItem.textContent = board[i];
-            cont.appendChild(gridItem);
+            boardDiv.appendChild(gridItem);
         }
+    })();
+
+    function updateGrid(index){
+        const clickedItem =  boardDiv.querySelector(`.grid-item[data-id="${index}"]`);
+        clickedItem.textContent = Gameboard.getBoard()[index];
     }
 
-    return {
-        board,
-        createGrid,
+    const updateMessage = () => {
+        messageDiv.querySelector('p').textContent = `It's ${GameController.getCurrentPlayer().getName()}'s turn.`;
+    };
+
+    updateMessage();
+
+    function playerClick(e) {
+        const index = +e.target.getAttribute('data-id')
+        GameController.playRound(index);
+        updateGrid(index);
+        updateMessage();
     }
+
+    boardDiv.addEventListener("click", playerClick)
+
 })();
 
-Gameboard.createGrid(document.querySelector('.container'));
+
