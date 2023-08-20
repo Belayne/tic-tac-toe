d86152015c21c0f0865701cb9732d5377674a6ce
Naol <37399395+Belayne@users.noreply.github.com>
2023-08-20 22:25:55 +0200

    Add win condition

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const Gameboard = (() => {
     const board = ["","","","","","","","",""];
 
@@ -21,31 +22,57 @@ const Player = (name, mark) => {
 }
 
 const GameController = (() => {
-    const playerOne = Player("One", "X")
-    const playerTwo = Player("Two", "O")
+    const playerOne = Player("Player One", "X")
+    const playerTwo = Player("Player Two", "O")
+    const WIN_CONDITIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     const board = Gameboard.getBoard();
+    const gameStatus = {
+        state: "Playing",
+        winner: ""
+    }
 
     let currentPlayer = playerOne;
 
     const switchPlayer = () => {
         currentPlayer = (currentPlayer == playerOne)? playerTwo: playerOne;
-        console.log(currentPlayer.getName())
     }
 
     const playRound = (index) => {
         if(!board[index]) {
             Gameboard.addMark(index, currentPlayer.getMark());
-            switchPlayer();
+            checkWin();
+            if(gameStatus.state == "Playing") {
+                switchPlayer();
+            }
         }
         else {
             console.log("Already Played")
         }
-        console.log(Gameboard.getBoard())
+        console.table(Gameboard.getBoard())
+        console.table(gameStatus);
     }
 
+    const checkWin = () => {
+        const playerPositions = board.map((a, index) => {
+            if(a === currentPlayer.getMark()) return index;
+        });
+
+        if(playerPositions.length > 2) {
+            if(WIN_CONDITIONS.some(winArr => winArr.every(a => playerPositions.includes(a)))) {
+                gameStatus.state = "Game Won"
+                gameStatus.winner = currentPlayer.getName();
+            }
+            else if(board.every(el => el != "")) {
+                gameStatus.state = "Draw"
+            }
+        }
+    }
+
+    const getGameStatus = () => gameStatus;
     const getCurrentPlayer = () => currentPlayer;
 
     return {
+        getGameStatus,
         getCurrentPlayer,
         playRound
     }
@@ -55,6 +82,13 @@ const GameController = (() => {
 const DisplayController = (() => {
     const boardDiv = document.querySelector(".board")
     const messageDiv = document.querySelector(".message");
+
+    const pickMessage = (state) => {
+        let message = (state === "Playing")? `It's ${GameController.getCurrentPlayer().getName()}'s turn.`:
+            (state === "Draw")? `It's a Draw!`: `${GameController.getGameStatus().winner} wins!`;
+        
+        return message;
+    }
 
     const createGrid = (() => {
         for(let i = 0; i < 9; i++){
@@ -71,16 +105,22 @@ const DisplayController = (() => {
     }
 
     const updateMessage = () => {
-        messageDiv.querySelector('p').textContent = `It's ${GameController.getCurrentPlayer().getName()}'s turn.`;
+        let gameState = GameController.getGameStatus().state;
+        let message = pickMessage(gameState);
+        
+        console.log(message)
+        messageDiv.querySelector('p').textContent = message;
     };
 
     updateMessage();
 
     function playerClick(e) {
-        const index = +e.target.getAttribute('data-id')
-        GameController.playRound(index);
-        updateGrid(index);
-        updateMessage();
+        if(GameController.getGameStatus().state === "Playing") {
+            const index = +e.target.getAttribute('data-id')
+            GameController.playRound(index);
+            updateGrid(index);
+            updateMessage();
+        }
     }
 
     boardDiv.addEventListener("click", playerClick)
